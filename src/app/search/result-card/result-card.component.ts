ab949846463ac591602d37061fb0666017e1c496
Simon Todorov <simon.todorov@mathema.de>
2024-07-19 12:06:10 +0200

    Done before routing

--- src/app/search/result-card/result-card.component.ts
+++ src/app/search/result-card/result-card.component.ts
@@ -8,8 +8,13 @@ import { Component, Input } from '@angular/core';
   styleUrl: './result-card.component.css'
 })
 export class ResultCardComponent {
+  showContent: boolean = false;
   @Input() imgCard: any;
   @Input() titleCard: any;
   @Input() descriptionCard: any;
   @Input() linkCard: any;
+
+  toggleCardContent() {
+    this.showContent = !this.showContent;
+  }
 }
--- src/app/search/search-ingredients/search-ingredients.component.ts
+++ src/app/search/search-ingredients/search-ingredients.component.ts
@@ -1,18 +1,29 @@
 import { Component, inject, NgModule } from '@angular/core';
 import { ResultCardComponent } from '../result-card/result-card.component';
 import { ApiFoodService } from '../../services/api-food.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-ingredients',
   standalone: true,
-  imports: [ResultCardComponent,],
+  imports: [ResultCardComponent, FormsModule],
   templateUrl: './search-ingredients.component.html',
   styleUrl: './search-ingredients.component.css'
 })
 export class SearchIngredientsComponent {
   private apiService = inject(ApiFoodService)
   recipeDetails: any;
-  search: string = "Fish";
+  private _search: string = '';
+
+
+  public get search(): string {
+    return this._search;
+  }
+
+  set search(s: string) {
+    this._search = s;
+    this.recipeDetails = [];
+  }
 
   getRecipeDetails() {
     this.apiService.getFoodURL(this.search).subscribe((data) => {
